9b00327d9b545f32c80398f0c92060bb70260b2d
Ledwin W <129959406+ledwinEzkiel@users.noreply.github.com>
2023-04-19 21:22:30 +0800

    Menambahkan file modul-1.py

---
+++ Semester-2/dasar-pemrograman-dan-struktur-data/Modul-1.py
@@ -0,0 +1,9 @@
+print("Nama: Ledwin")
+print("5720122028")
+print("Angkatan 2023")
+print("Program Studi: Sistem Informasi")
+print("STMIK Adhi Guna")
+
+print('Budi kuliah di STMIK Adhi Guna) #kuliah budi
+
+prnt("lsdjsdeie")
