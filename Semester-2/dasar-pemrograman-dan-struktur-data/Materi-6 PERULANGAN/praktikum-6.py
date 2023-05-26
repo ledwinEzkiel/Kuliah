8ae3bbc393d7cbb9d56ec9edb234e1080a8e7dc6
Ledwin W <ledwinezkielwotulo@gmail.com>
2023-05-26 09:02:26 +0800

    Perubahan program

--- Semester-2/dasar-pemrograman-dan-struktur-data/Materi-6 PERULANGAN/praktikum-6.py
+++ Semester-2/dasar-pemrograman-dan-struktur-data/Materi-6 PERULANGAN/praktikum-6.py
@@ -1,31 +1,34 @@
 # program 1
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print("prgram 1\n")
+numbers = int(input("data n: "))
 genap = 0
 ganjil = 0
 i = 0
 
-while i < len(numbers):
-    if numbers[i] % 2 == 0:
+while (i < numbers):
+    i += 1
+    print(i, end=" ")
+    if i % 2 == 0:
         genap += 1
     else:
         ganjil += 1
-    i += 1
 
-print("List angka:", numbers)
-print("Jumlah bilangan genap:", genap)
+print("\nJumlah bilangan genap:", genap)
 print("Jumlah bilangan ganjil:", ganjil)
 
 
 # program 2
-number = int(input("Masukan angka: "))
+print("\nprgram 2\n")
+number = int(input("\nMasukan angka: "))
 for i in range(1, 11):
-    print(number, "*", i, "=", number*i)
+    print(number, "*", i, "=", number*i, )
 
 # program 3
-
+print("\nprgram 3\n")
 n = 11
 a, b = 0, 1
 
 for i in range(n):
     print(b, end=" ")
     a, b = b, a + b
+print("\n")
